7cc2cd74b5f8412014e4a1eeb388245b195ede74
shashwatrauthan <shashwatrauthan@gmail.com>
2022-05-20 09:00:29 +0530

    Initial

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const http = require('http')
+
+const app = express()
+const port = 3000
+
+const server = http.createServer(app);
+
+app.use(express.static(__dirname + "/public"))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html")
+})
+
+server.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+// Socket.io
+
+const io = require("socket.io")(server);
+
+io.on("connection",(socket)=>{
+    console.log("Connected...");
+    
+    socket.on("message",(msg)=>{
+        socket.broadcast.emit("message",msg);
+    })
+})
---
+++ public/script.js
@@ -0,0 +1,60 @@
+const socket = io();
+
+let messageSection = document.querySelector('.message_section');
+// Username
+let u_name;
+
+do
+{
+    u_name = prompt("Enter your name.");
+}while(!u_name);
+
+
+// Outgoing Messages
+let textarea = document.querySelector('.message_box');
+
+textarea.addEventListener("keyup",(e)=>{
+    if(e.key === "Enter")
+    {
+        msg = {
+            name : u_name,
+            message : e.target.value.trim()
+        }
+
+        e.target.value = "";
+        // append
+        addMsg(msg,"outgoing")
+
+        // send to server
+        socket.emit("message",msg);
+
+    }
+})
+
+
+function addMsg(msg,type)
+{
+    let elem_div = document.createElement("div");
+    elem_div.classList.add("message" , type);
+    
+    let string = `<h5>${msg.name}</h5>
+    <p>${msg.message}</p>`
+    elem_div.innerHTML = string;
+
+    let messageSection = document.querySelector('.message_section');
+    messageSection.appendChild(elem_div)
+
+    scrollToBottom()
+}
+
+
+// Receiving Messages
+socket.on("message",(msg)=>{
+    addMsg(msg,"incoming");
+})
+
+// Scroll Function
+function scrollToBottom()
+{
+    messageSection.scrollTop = messageSection.scrollHeight;
+}
