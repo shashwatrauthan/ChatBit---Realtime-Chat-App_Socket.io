860d98be43017429b8310cc5602e1ff392d91428
shashwatrauthan <shashwatrauthan@gmail.com>
2022-05-20 09:05:13 +0530

    Public folder updated

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 app.use(express.static(__dirname + "/public"))
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + "/index.html")
+  res.sendFile(__dirname + "../public/index.html")
 })
 
 server.listen(port, () => {
