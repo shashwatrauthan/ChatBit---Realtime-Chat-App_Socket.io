ecb9a226905377da93b250acb53540c56158da15
shashwatrauthan <shashwatrauthan@gmail.com>
2022-05-20 09:19:12 +0530

    Port Updates

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const http = require('http')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 const server = http.createServer(app);
 
